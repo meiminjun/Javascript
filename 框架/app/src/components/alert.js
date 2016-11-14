3a879ff776f92950915570530b9a2c30c1aadd76
Apple <apple@AppledeMacBook-Air-2.local>
2016-11-14 11:49:52 +0800

    优化改动

--- 框架/app/src/components/alert.js
+++ 框架/app/src/components/alert.js
@@ -1,9 +1,38 @@
-define(function() {
+define(['zepto'],function($) {
     'use strict';
-    function Window() {}
+    function Window(option) {
+        this.cfg = {
+            width:500,
+            height:300,
+            title:"系统消息",
+            content:'',
+            handler:null,
+            hasCloseBtn:false,
+            skinClassName:null,
+            text4AlertBtn:'确定',
+            handlerAlertBtn:null,
+            handlerCloseBtn:null
+        };
+    }
 
     Window.prototype = {
-      alert:function() {},
+      alert:function(cfg) {
+          var CFG = $.extend(this.cfg,cfg),
+              boundingBox = $('<div class="window_boundindBox"><div class="window_header">'+CFG.title+'</div><div class="window_body">'+CFG.content+'</div><div class="window_footer"><button class="window_button_confirm">'+CFG.text4AlertBtn+'</button></div></div>'),
+          btn = boundingBox.find(".window_button_confirm");
+          boundingBox.appendTo('body');
+
+          btn.click(function() {
+            CFG.handler && CFG.handler();
+            boundingBox.remove();
+          });
+          boundingBox.css({
+            width:CFG.width + 'px',
+            height:CFG.height + 'px',
+            // left:(CFG.x||(window.innerWidth - CFG.width)/2)+'px',
+            // top:(CFG.y||(window.innerHeight - CFG.height)/2)+'px'
+          })
+      },
       comfirm:function() {}
     };
     return {
--- 框架/app/src/main.js
+++ 框架/app/src/main.js
@@ -1,20 +1,68 @@
 /*
  * APP全局上下文环境参数配置
-*/
+ */
 require.config({
-    baseUrl :"",
-    shim    : {
-        'zepto' : {
-            exports : '$'
-        }
-    },
-    paths : {
-        zepto : 'libs/zepto/v1.1.6/zepto.min',
-        components:'src/components'
+  baseUrl: "",
+  shim: {
+    'zepto': {
+      exports: '$'
     }
+  },
+  paths: {
+    zepto: 'libs/zepto/v1.1.6/zepto.min',
+    components: 'src/components'
+  }
 });
 
-require(['zepto','components/alert'],function($,alert) {
-   var body = $("body");
-   console.log(body);
+require(['zepto', 'components/alert', 'components/Tab'], function($, w, t) {
+  var body = $("body");
+  console.log(body);
+  // var AlertView = new w.Window().alert({
+  //      title:'提示',
+  //      content:'这是一个提示内容',
+  //      text4AlertBtn:'取消',
+  //      handler:function() {
+  //          alert(1111);
+  //      }
+  //  });
+
+  var tab = new t.Tab().init('main', [{
+    text: '222sdf',
+    handler: function(ele, index) {
+      alert("开始请求数据");
+      getData();
+    },
+    targetDomId: 'tabOne',
+  }, {
+    text: '测试',
+    handler: function(ele, index) {
+      // alert(11);
+    },
+    targetDomId: 'tabTwo',
+    contentHeight: 55,
+  }, {
+    text: 'dfadf',
+    handler: function(ele, index) {
+      // alert(22);
+    },
+    targetDomId: 'tabThree',
+    contentHeight: 55,
+  }, {
+    text: '首页',
+    handler: function(ele, index) {
+      // alert(33);
+    },
+    targetDomId: 'tabFour',
+    contentHeight: 55,
+  }])
+
+  function getData() {
+    $.getJSON('./mockData.json', function(data) {
+      alert('成功返回数据');
+      // Supposing this JSON payload was received:
+      //   {"project": {"id": 42, "html": "<div>..." }}
+      // append the HTML to context object.
+      alert(data);
+    })
+  }
 });
