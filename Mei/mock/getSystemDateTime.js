a59b24b63b805a16590646272a94a86ccc6f47cc
meiminjun <251222845@qq.com>
2017-04-08 15:33:02 +0800

    删除文件

--- Mei/mock/getSystemDateTime.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: '获取服务器时间',
-  env: 'default',
-  response: function (req, res) {
-    res.json({"code": "000000", "data": {"current_date_time": 1490007705034}, "msg": "success"})
-  }
-}
